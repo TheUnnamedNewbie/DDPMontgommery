a5840be9511a2fc25780e88b91702352b15e717e
TheUnnamedNewbie <joren.vaes@gmail.com>
2016-10-30 22:41:23 +0100

    Turned inner loops (including for() ) into asm
    Turned inner loops into assembly. Down to 27k cylces. Just have to
    expand the outwer two loops and then we are done

--- src/montgomery.c
+++ src/montgomery.c
@@ -47,44 +47,37 @@ void fips(uint32_t *a, uint32_t *b, uint32_t *n, uint32_t *n0, uint32_t *res, ui
 
 	uint32_t t[3];
 	uint32_t m[SIZE+1];
+	//uint64_t resultAsm;
+	//uint32_t S, C;
 	int i, j;
 	t[2]=t[1]=t[0]=0;
-
 	for(i = 0; i<= SIZE; i++){
 			m[i] = 0;
 	}
-
-
-	//uint64_t sum;
-	//uint32_t S, C;
-
 	for(i=0; i< SIZE; i++){
-		for(j = 0; j< i; j++){
-			montSum(a[j], b[i-j], m[j], n[i-j], t);
-		}
+			montSumLoop(a, b, m, n, t, i); //IT HAS TO STILL SKIP FIRST ITERATION
+			//S = (uint32_t)resultAsm;			//TEMP THIS IS NOT HOW THE REAL CODE SHOULD WORK
+			//C = (uint32_t)(resultAsm>>32);
+			//LogWithNumH(ASMMONTGOMERY, DEBUG, "loop i:", S);
+			//LogWithNumH(ASMMONTGOMERY, DEBUG, "loop j:", C);
+
 			montSum2(a[i], b[0], m[i], n[0], t, m, i, n0[0]);
 	}
-
 	for(i = SIZE; i < 2*SIZE; i++){  // ASUMING THAT SIZE WILL NEVER BE MORE THAN 31 BITS LONG
-
-		for(j = (i - SIZE + 1); j < SIZE; j++){
-				montSum(a[j], b[i-j], m[j], n[i-j], t);
-		}
-
+		//for(j = (i - SIZE + 1); j < SIZE; j++){
+		montSumLoopSize(a, b, m, n, t, i, (i-SIZE+1));
+		//}
 		m[i - SIZE] = t[0];
-
 		t[0] = t[1];
 		t[1] = t[2];
 		t[2] = 0;
-
 	}
-
 	m[SIZE] = t[0];
-	uint32_t startsubcond = get_cycle_counter();
-	subCondMont(m, n, SIZE);
-	uint32_t stopsubcond =  get_cycle_counter();
 
-	LogWithNum(MONTGOMMERY, CRITICAL, "Cycles for subcond: ", stopsubcond - startsubcond);
+	subCondMont(m, n, SIZE);
+
+
+
 	for(i = 0; i <SIZE; i++){
 		res[i] = m[i];
 	}
